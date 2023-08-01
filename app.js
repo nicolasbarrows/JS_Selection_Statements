02b1ee529eddf170a35c5fe85d31f070fe8d1783
Nicolas Barrows <contact@nicolasbarrows.com>
2023-08-01 13:16:58 -0500

    COMPLETED: Exercise 1

--- app.js
+++ app.js
@@ -2,3 +2,16 @@ console.log("Hello World!\n==========\n");
 console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
+
+let myNumber = 27
+
+let inputNumber = prompt("Pick a number.");
+
+if (inputNumber < myNumber ) {
+  console.log("too low");
+} else if (inputNumber > myNumber) {
+  console.log("too high");
+} else {
+  console.log("Congratulations!!!");
+}
+
