3a6f68d4f306703576dbeae53b8c32eff59c270d
Nicolas Barrows <contact@nicolasbarrows.com>
2023-08-01 16:53:07 -0500

    COMPLETED: Exercise 2

--- app.js
+++ app.js
@@ -8,10 +8,41 @@ let myNumber = 27
 let inputNumber = prompt("Pick a number.");
 
 if (inputNumber < myNumber ) {
-  console.log("too low");
+  // console.log("too low");
 } else if (inputNumber > myNumber) {
-  console.log("too high");
+  // console.log("too high");
 } else {
-  console.log("Congratulations!!!");
+  // console.log("Congratulations!!!");
 }
+
+let birthMonth = window.prompt("What is your birth month?");
+ 
+let birthSeason;
+
+switch (birthMonth.toLowerCase()) {
+  case "january":
+  case "february":
+  case "december":
+    birthSeason = "Winter";
+    break;
+  case "march":
+  case "april":
+  case "may":
+    birthSeason = "Spring";
+    break;
+  case "june":
+  case "july":
+  case "august":
+    birthSeason = "Summer";
+    break;
+  case "september":
+  case "october":
+  case "november":
+    birthSeason = "Fall";
+    break;
+  default:
+    console.log("incorrect format. please try again.");
+}
+console.log("You said: " + birthMonth)
+console.log("You were born in the " + birthSeason)
 
